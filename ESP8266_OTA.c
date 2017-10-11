e128af3a253140a39699446a99892994d8968634
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-10-11 18:25:22 +0530

    initial project files commit

---
+++ ESP8266_OTA.c
@@ -0,0 +1,17 @@
+/****************************************************************
+* ESP8266 OTA UPDATE LIBRARY
+*
+* REQUIRES rBoot
+*
+* OCTOBER 11 2017
+*
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ------------
+*		(1) rBoot
+*				https://github.com/raburton/rboot
+****************************************************************/
+
+#include "ESP8266_OTA.h"
---
+++ ESP8266_OTA.h
@@ -0,0 +1,29 @@
+/****************************************************************
+* ESP8266 OTA UPDATE LIBRARY
+*
+* REQUIRES rBoot
+*
+* OCTOBER 11 2017
+*
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ------------
+*		(1) rBoot
+*				https://github.com/raburton/rboot
+****************************************************************/
+
+#ifndef _ESP8266_OTA_H_
+#define _ESP8266_OTA_H_
+
+#include "osapi.h"
+#include "mem.h"
+#include "ets_sys.h"
+#include "ip_addr.h"
+#include "espconn.h"
+#include "os_type.h"
+
+
+
+#endif
