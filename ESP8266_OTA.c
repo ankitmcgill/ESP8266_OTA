9aca6ecae7f9cc332f6ebd2c8d370dbfaf1291ba
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-10-11 18:57:19 +0530

    Initial repo files commit with basic code

--- ESP8266_OTA.c
+++ ESP8266_OTA.c
@@ -12,6 +12,60 @@
 * ------------
 *		(1) rBoot
 *				https://github.com/raburton/rboot
+*				https://richard.burtons.org/2015/05/18/rboot-a-new-boot-loader-for-esp8266/
+*				https://richard.burtons.org/2015/05/23/rboot-tutorial-for-esp8266-ota-updates/
 ****************************************************************/
 
 #include "ESP8266_OTA.h"
+
+//LOCAL LIBRARY VARIABLES/////////////////////////////////////
+//DEBUG RELATED
+static uint8_t _esp8266_ota_debug;
+
+//OTA SERVER IP RELATED
+static ip_addr_t _esp8266_ota_server_ip;
+static ip_addr_t _esp8266_ota_server_port;
+static char* _esp8266_ota_server_path;
+
+//CALLBACK FUNCTION POINTERS
+static void (*_esp8266_ota_done_user_cb)(bool);
+//END LOCAL LIBRARY VARIABLES/////////////////////////////////
+
+//CONFIGURATION FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_OTA_SetDebug(uint8_t debug_on)
+{
+    //SET DEBUG PRINTF ON(1) OR OFF(0)
+
+    _esp8266_ota_debug = debug_on;
+}
+
+void ICACHE_FLASH_ATTR ESP8266_OTA_Initialize(ip_addr_t server_ip, uint16_t server_port, char* server_path)
+{
+    //INITIALIZE ESP8266 OTA PARAMETERS
+
+    _esp8266_ota_server_ip = server_ip;
+    _esp8266_ota_server_port = server_port;
+    _esp8266_ota_server_path = server_path;
+
+    if(_esp8266_ota_debug)
+	{
+		os_printf("ESP8266 : OTA : Initialized ota with specified!\n");
+		os_printf("ESP8266 : OTA : server ip = %d.%d.%d.%d\n", IP2STR(&_esp8266_ota_server_ip));
+		os_printf("ESP8266 : OTA : server port = %u\n", _esp8266_ota_server_port);
+		os_printf("ESP8266 : OTA : server path = %s\n", _esp8266_ota_server_path);
+	}
+}
+
+void ICACHE_FLASH_ATTR ESP8266_OTA_SetCallbackFunctions(void (*ota_done_user_cb)(bool))
+{
+    //SET USER CB FUNCTION
+
+    if(ota_done_user_cb != NULL)
+    {
+        _esp8266_ota_done_user_cb = ota_done_user_cb;
+        if(_esp8266_ota_debug)
+        {
+            os_printf("ESP8266 : OTA : User cb function set!\n");
+        }
+    }
+}
--- ESP8266_OTA.h
+++ ESP8266_OTA.h
@@ -12,6 +12,8 @@
 * ------------
 *		(1) rBoot
 *				https://github.com/raburton/rboot
+*				https://richard.burtons.org/2015/05/18/rboot-a-new-boot-loader-for-esp8266/
+*				https://richard.burtons.org/2015/05/23/rboot-tutorial-for-esp8266-ota-updates/
 ****************************************************************/
 
 #ifndef _ESP8266_OTA_H_
@@ -23,7 +25,30 @@
 #include "ip_addr.h"
 #include "espconn.h"
 #include "os_type.h"
+#include "rboot/rboot-ota.h"
 
 
+#define ESP8266_OTA_HTTP_HEADER     "Connection: keep-alive\r\n"
+                                    "Cache-Control: no-cache\r\n"
+                                    "User-Agent: rBoot-Sample/1.0\r\n"
+                                    "Accept: */*\r\n\r\n"
+#define ESP8266_OTA_HTTP_STRING     "GET %s%s HTTP/1.1\r\nHost: "IPSTR"\r\n"
 
+//CUSTOM VARIABLE STRUCTURES/////////////////////////////
+//END CUSTOM VARIABLE STRUCTURES/////////////////////////
+
+//FUNCTION PROTOTYPES/////////////////////////////////////
+//CONFIGURATION FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_OTA_SetDebug(uint8_t debug_on);
+void ICACHE_FLASH_ATTR ESP8266_OTA_Initialize(ip_addr_t server_ip,
+											    uint16_t server_port,
+												char* server_path);
+void ICACHE_FLASH_ATTR ESP8266_OTA_SetCallbackFunctions(void (*ota_done_user_cb)(bool));
+
+//CONTROL FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_OTA_Start(void);
+
+//INTERNAL CALLBACK FUNCTIONS
+void ICACHE_FLASH_ATTR _esp8266_ota_done_cb(void *arg, bool result);
+//END FUNCTION PROTOTYPES/////////////////////////////////
 #endif
